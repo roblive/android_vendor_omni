d08202fffe872d36835e7b4200a5f3d3c047ac55
Rob Live <roblive@users.noreply.github.com>
2014-01-05 02:09:20 +0100

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -1,34 +1,8 @@
 PRODUCT_BRAND ?= omni
 
 # bootanimation
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,, $(shell ls vendor/omni/prebuilt/bootanimation))
-bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
-
-# find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
-
-PRODUCT_BOOTANIMATION := vendor/omni/prebuilt/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-endif
+PRODUCT_COPY_FILES += \
+        vendor/omni/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # general properties
 PRODUCT_PROPERTIES_OVERRIDE += \
@@ -61,10 +35,6 @@ PRODUCT_COPY_FILES += \
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
-    
-# Wallpaper APP
-PRODUCT_COPY_FILES += \
-        vendor/omni/prebuilt/apk/TeamUBWallpapers.apk:system/app/TeamUBWallpapers.apk    
 
 # Init script file with omni extras
 PRODUCT_COPY_FILES += \
